6e55e5ff94901db72b05cc49d0c2b206577d7d0d
Ilelaboye Ayomide <ilelaboyeayomide8@gmail.com>
2026-08-07 10:44:21 +0100

    Created Contact component and added it to the page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,3 +1,31 @@
 export default function Contact() {
-  return <div></div>;
+  return (
+    <div>
+      <h2>Contact</h2>
+      <div>
+        <h3>
+          Email:{" "}
+          <a href="mailto:ilelaboyeayomide8@gmail.com?subject=Portfolio Inquiry&amp;body=Hello Ayomide, I would like to discuss a project with you.">
+            {" "}
+            ilelaboyeayomide8@gmail.com
+          </a>
+        </h3>
+        <h3>
+          WhatsApp:{" "}
+          <a
+            href="https://wa.me/2349113519042?text=Hello%20Ayomide%2C%20I%20came%20across%20your%20portfolio%20and%20I%27m%20interested%20in%20working%20with%20you.%20I%27d%20love%20to%20discuss%20a%20potential%20project%20with%20you."
+            target="_blank"
+          >
+            +234 911 351 9042
+          </a>
+        </h3>
+        <h3>
+          LinkedIn:{" "}
+          <a href="https://www.linkedin.com/in/ilelaboye-ayomide-2bb7853a3/">
+            Ilelaboye Ayomide
+          </a>
+        </h3>
+      </div>
+    </div>
+  );
 }
