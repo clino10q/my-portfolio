e7cb5384709b965e72f708dd8aa3a5f56dd46c81
Ilelaboye Ayomide <ilelaboyeayomide8@gmail.com>
2026-08-07 11:24:36 +0100

    Created the footer and changed the favicon

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,7 +7,7 @@ export default function Contact() {
           Email:{" "}
           <a
             href="mailto:ilelaboyeayomide8@gmail.com?subject=Portfolio Inquiry&amp;body=Hello Ayomide, I would like to discuss a project with you."
-            className="text-sm text-blue-600"
+            className="text-sm text-blue-600 hover:underline"
           >
             {" "}
             ilelaboyeayomide8@gmail.com
@@ -17,7 +17,7 @@ export default function Contact() {
           WhatsApp:{" "}
           <a
             href="https://wa.me/2349113519042?text=Hello%20Ayomide%2C%20I%20came%20across%20your%20portfolio%20and%20I%27m%20interested%20in%20working%20with%20you.%20I%27d%20love%20to%20discuss%20a%20potential%20project%20with%20you."
-            className="text-sm text-blue-600"
+            className="text-sm text-blue-600 hover:underline"
             target="_blank"
           >
             +234 911 351 9042
@@ -27,7 +27,7 @@ export default function Contact() {
           LinkedIn:{" "}
           <a
             href="https://www.linkedin.com/in/ilelaboye-ayomide-2bb7853a3/"
-            className="text-sm text-blue-600"
+            className="text-sm text-blue-600 hover:underline"
           >
             Ilelaboye Ayomide
           </a>
